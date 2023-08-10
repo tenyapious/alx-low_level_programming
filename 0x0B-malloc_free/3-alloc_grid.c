912d67c5c95b9de5d10ce5e68a2f9b8d80b778f1
tenyapious <tenyapious@gmail.com>
2023-08-10 10:06:24 -0700

    Free everything if malloc fails

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,6 +24,11 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = malloc(width * sizeof(int));
+		if (arr[i] == NULL)
+		{
+			free(arr);
+			return (NULL);
+		}
 	}
 
 	for (i = 0; i < height; i++)
