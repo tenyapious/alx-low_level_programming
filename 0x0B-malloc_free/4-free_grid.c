80f86863eacaf83cd63615bc71dfd03c2b3aa203
tenyapious <tenyapious@gmail.com>
2023-08-10 10:36:04 -0700

    write a fn that frees a 2d grid in alloc_grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - free allocated memory of a 2d grid
+ * @grid: the 2d grid
+ * @height: height of the grid
+*/
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
