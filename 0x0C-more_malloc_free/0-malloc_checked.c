0cc8655ce0d6e73f5e2cc0191321e56ba0779730
tenyapious <tenyapious@gmail.com>
2023-08-10 17:51:32 -0700

    write a fn that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory
+ * @b: size of memory to allocate
+ *
+ * Return: pointer to allocated memory
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,8 @@
+#ifndef FUNC_PROTO
+#define FUNC_PROTO
+
+char _putchar(char c);
+int _strLen(char *str);
+void *malloc_checked(unsigned int b);
+
+#endif
