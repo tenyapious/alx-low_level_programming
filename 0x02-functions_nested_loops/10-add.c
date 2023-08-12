93c6772c89e9de93564bcad60565628bae7ea84d
tenyapious <tenyapious@gmail.com>
2023-08-12 08:48:47 -0700

    write a description for add()

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * add - adds 2 ints
+ * @l: left operand int
+ * @r: right operand int
+ *
+ * Return: the sum of l and r
+ */
 int add(int l, int r)
 {
 	return (l + r);
