81fbf3e21976713648d5e976c4f39baa674d8ec1
tenyapious <tenyapious@gmail.com>
2023-08-16 22:16:19 -0700

    return -1 if array is NULL

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,6 +18,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (cmp == NULL)
 		return (-1);
 
+	if (array == NULL)
+		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) == 1)
