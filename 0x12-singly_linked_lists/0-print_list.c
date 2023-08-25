ae6c5a77fdd65f72d89b30a7e3bf42fdc635d2c0
tenyapious <tenyapious@gmail.com>
2023-08-24 22:19:08 -0700

    Write a fn that prints all the elements of a list_t list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - print elements in a linked list
+ * @h: head of linked list
+ *
+ * Return: length of linked list
+*/
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while (h->next != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		i++;
+	}
+	printf("[%d] %s\n", h->len, h->str);
+
+	return (++i);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,27 @@
+#ifndef LIST_S
+#define LIST_S
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ * *
+ *  Description: singly linked list node structure
+ **/
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+#endif
+
 #ifndef PUTCHAR
 #define PUTCHAR
-int _putchar(
+int _putchar(char c);
 #endif
 
 #ifndef PRINT_LIST
-#define PRINT_lIST
+#define PRINT_LIST
 size_t print_list(const list_t *h);
 #endif
