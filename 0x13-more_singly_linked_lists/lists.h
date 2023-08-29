4ff1fd4370b688f7ce133aa69b5ba3cc8f04828c
tenyapious <tenyapious@gmail.com>
2023-08-29 00:56:09 +0000

    Write a fn that frees a linst link list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -44,3 +44,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 #define FREE_LISTINT
 void free_listint(listint_t *head);
 #endif
+
+#ifndef FREE_LISTINT2
+#define FREE_LISTINT2
+void free_listint2(listint_t **head);
+#endif
---
+++ 0x13-more_singly_linked_lists/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int *i;
+
+	i = NULL;
+
+	printf("%p\n", (void *)i);
+
+	return (0);
+}
