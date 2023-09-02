cadffc94e510949e83ac9645d0184b0ca35cb020
tenyapious <tenyapious@gmail.com>
2023-09-02 18:24:12 +0000

    don't check if int value is less than index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,18 +7,16 @@
  * @index: given index
  *
  * Return: value of bit or -1
-*/
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if ((sizeof(n) * 8) < (index + 1))
+	if ((sizeof(n) * 8) < index)
 		return (-1);
 
-	if ((n >> index) > 0)
-	{
-		if ((n >> index) & 1)
-			return (1);
-		else
-			return (0);
-	}
+	if ((n >> index) & 1)
+		return (1);
+	else
+		return (0);
+
 	return (-1);
 }
