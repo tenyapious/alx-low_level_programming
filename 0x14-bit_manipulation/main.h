96c46ea78187bb529c8a0faa87fb88137dc0dc54
tenyapious <tenyapious@gmail.com>
2023-09-02 05:33:12 +0000

    write a fn that prints the binary representation of a number

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,4 +1,14 @@
+#ifndef _PUTCHAR
+#define _PUTCHAR
+int _putchar(char c);
+#endif
+
 #ifndef BINARY_TO_UINT
 #define BINARY_TO_UINT
 unsigned int binary_to_uint(const char *b);
+#endif
+
+#ifndef PRINT_BINARY
+#define PRINT_BINARY
+void print_binary(unsigned long int n);
 #endif
