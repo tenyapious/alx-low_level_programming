24e47c615dceef3143e1690fe44b540ca6200c30
tenyapious <tenyapious@gmail.com>
2023-09-06 15:46:08 +0000

    write a fn that create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * create_file - creates a file
+ * @filename: name for the file
+ * @text_content: contents to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd, byte_written;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (strcmp(text_content, "NULL") == 0)
+		text_content = "";
+
+	byte_written = write(fd, text_content, strlen(text_content));
+
+	if (byte_written == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,3 +2,8 @@
 #define READ_TEXTFILE
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif
+
+#ifndef CREATE_FILE
+#define CREATE_FILE
+int create_file(const char *filename, char *text_content);
+#endif
