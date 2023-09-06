b6c21c029b7ec3d516446f4c1d6bc83d440546ad
tenyapious <tenyapious@gmail.com>
2023-09-06 21:52:28 +0000

    return -1 if text_content is NULL

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (strcmp(text_content, "NULL") == 0)
+	if (text_content == NULL || strcmp(text_content, "NULL") == 0)
 		text_content = "";
 
 	byte_written = write(fd, text_content, strlen(text_content));
