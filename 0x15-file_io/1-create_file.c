a209f4af9af95e0523974c06601ea2586199936f
tenyapious <tenyapious@gmail.com>
2023-09-06 21:41:44 +0000

    create empty file if text_content is NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,14 +14,14 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, byte_written;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	if (strcmp(text_content, "NULL") == 0)
+	if (text_content == NULL || strcmp(text_content, "NULL") == 0)
 		text_content = "";
 
 	byte_written = write(fd, text_content, strlen(text_content));
