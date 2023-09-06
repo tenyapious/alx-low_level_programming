3c208f5e9b58bee43640c43b1d8a33f9f0bde6b6
tenyapious <tenyapious@gmail.com>
2023-09-06 21:35:08 +0000

    check if text content is NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, byte_written;
 
-	if (filename == NULL)
+	if (filename == NULL || text_content == NULL)
 		return (-1);
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
@@ -25,7 +25,6 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 
 	byte_written = write(fd, text_content, strlen(text_content));
-
 	if (byte_written == -1)
 		return (-1);
 
