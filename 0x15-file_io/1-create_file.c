7784244a110b67e7dfbacc35c86e276e5a134e07
tenyapious <tenyapious@gmail.com>
2023-09-06 16:12:30 +0000

    check if open was successful

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,6 +18,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 
 	if (strcmp(text_content, "NULL") == 0)
 		text_content = "";
