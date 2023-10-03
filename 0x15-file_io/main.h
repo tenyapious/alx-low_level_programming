5f6c92de223b0e88d6dd5f42e96e38e3bad1aae2
tenyapious <tenyapious@gmail.com>
2023-10-02 23:08:03 -0500

    Write a fn that create a file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,3 +7,8 @@ int _putchar(char c);
 #define _READ_TEXTFILE_H
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif
+
+#ifndef CREATE_FILE
+#define CREATE_FILE
+int create_file(const char *filename, char *text_content);
+#endif
