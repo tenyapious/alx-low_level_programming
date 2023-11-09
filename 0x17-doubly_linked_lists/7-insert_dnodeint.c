8e1d29a8c9c89c19400a43137b0b2ebaad7fcde2
tenyapious <tenyapious@gmail.com>
2023-11-09 16:19:06 +0000

    insert node at list end

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,6 +26,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (temp == *h)
 		new_node = add_dnodeint(h, n);
+	else if (temp->next == NULL)
+	{
+		new_node = add_dnodeint_end(h, n);
+	}
 	else
 	{
 		temp1 = temp->prev;
