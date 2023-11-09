911b79c34bb697d4273c558975239d21ecd97242
tenyapious <tenyapious@gmail.com>
2023-11-09 18:07:47 +0000

    Rewrite code

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -2,34 +2,42 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * delete_dnodeint_at_index - delete node at index of dlistint_t list
+ * @head: pointer to first node of list
+ * @index: index of node to delete
+ *
+ * Return: 1 if successful or -1
+*/
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *temp, *f_pt, *l_pt;
-	unsigned int i;
 
 	temp = *head;
-	for (i = 0; i < index; i++)
+	while (temp && index)
 	{
-		if (temp)
-			temp = temp->next;
-		else
-			return (-1);
+		temp = temp->next;
+		index--;
 	}
+	if (temp == NULL || index)
+		return (-1);
 
 	f_pt = temp->prev;
 	l_pt = temp->next;
 
-	if (f_pt == NULL && l_pt == NULL)
-		*head = NULL;
-	else if (f_pt == NULL)
-	{
-		l_pt->prev = NULL;
-		*head = l_pt;
-	}
-	else if (l_pt == NULL)
+	if (temp == *head)
+		*head = temp->next;
+	else if (temp->next == NULL)
 		f_pt->next = NULL;
-	free(temp);	
-
+	else
+	{
+		f_pt->next = l_pt;
+		l_pt->prev = f_pt;
+	}
+	free(temp);
+	temp = NULL;
+	f_pt = NULL;
+	l_pt = NULL;
 
 	return (1);
 }
