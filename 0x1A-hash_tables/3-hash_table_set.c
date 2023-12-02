9ea65321f58de3232f5a4d065e6b9e60956140ff
tenyapious <tenyapious@gmail.com>
2023-12-02 20:33:18 +0000

    remove trailing whitespace

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,7 +10,7 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t **array, *node;
+	hash_node_t *node;
 	unsigned long int index;
 
 	node = malloc(sizeof(hash_node_t));
@@ -20,11 +20,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (strlen(key) == 0 || key == NULL)
 		return (0);
 
-	
-
 	index = key_index((unsigned char *)key, ht->size);
 
-	node->key = key;
+	node->key = strcpy(node->key, key);
 	node->value = strdup(value);
 	node->next = ht->array[index];
 	ht->array[index] = node;
