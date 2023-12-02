46fe9cee7ee6e9583beb7cf62a118e228db80b15
tenyapious <tenyapious@gmail.com>
2023-12-02 11:27:38 +0000

    Declare array as a pointer pointer

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
-	hash_node_t *array;
+	hash_node_t **array;
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
@@ -22,7 +22,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	hash_table->size = size;
-	hash_table->array = &array;
+	hash_table->array = array;
 
 	return (hash_table);
 }
