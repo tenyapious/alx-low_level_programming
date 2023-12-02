4262f9f28b890ed106d9349449d25305a135c9d2
tenyapious <tenyapious@gmail.com>
2023-12-02 16:16:06 +0000

    unhash key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,7 +24,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((unsigned char *)key, ht->size);
 
-	node->key = hash_djb2((unsigned char *)key);
+	node->key = key;
 	node->value = strdup(value);
 	node->next = ht->array[index];
 	ht->array[index] = node;
