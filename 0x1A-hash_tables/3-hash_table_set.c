8c407b250b1d3119054b34bd75c7a8141f8e4b4f
tenyapious <tenyapious@gmail.com>
2023-12-02 21:52:56 +0000

    assign first node's next to NULL

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -30,7 +30,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node->value = valueDup;
 
 	if (ht->array[index] == NULL)
+	{
+		node->next = NULL;
 		ht->array[index] = node;
+	}
 	else
 	{
 		node->next = ht->array[index];
