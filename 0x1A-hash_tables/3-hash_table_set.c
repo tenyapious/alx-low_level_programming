cc79006f8c3f673ad1a72f288e87daa5cc4a7e4d
tenyapious <tenyapious@gmail.com>
2023-12-02 16:15:01 +0000

    hash key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,7 +24,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((unsigned char *)key, ht->size);
 
-	node->key = key;
+	node->key = hash_djb2((unsigned char *)key);
 	node->value = strdup(value);
 	node->next = ht->array[index];
 	ht->array[index] = node;
