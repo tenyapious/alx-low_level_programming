b0a1383b067ac8d4a34ed2e1d28c538fa47fa266
tenyapious <tenyapious@gmail.com>
2023-12-06 14:54:31 +0000

    Back to 4 days ago

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,4 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_set - set a value to a hash table
  * @ht: the hash table
@@ -10,34 +9,29 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node, *temp;
+	hash_node_t *node;
 	unsigned long int index;
 	char *keyDup, *valueDup;
-
-	if (ht == NULL || key == NULL || strlen(key) == 0 || value == NULL)
-		return (0);
-
-	index = key_index((unsigned char *)key, ht->size);
-
-	keyDup = strdup(key);
-	valueDup = strdup(value);
-	if (keyDup == NULL || valueDup == NULL)
-		return (0);
-
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
-	{
-		free(keyDup);
-		free(valueDup);
 		return (0);
-	}
-
+	if (ht == NULL || key == NULL || strlen(key) == 0)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	keyDup = strdup(key);
+	valueDup = strdup(value);
 	node->key = keyDup;
 	node->value = valueDup;
 
-	temp = ht->array[index];
-	node->next = temp;
-	ht->array[index] = node;
-
+	if (ht->array[index] == NULL)
+	{
+		node->next = NULL;
+		ht->array[index] = node;
+	}
+	else
+	{
+		node->next = ht->array[index];
+		ht->array[index] = node;
+	}
 	return (1);
 }
