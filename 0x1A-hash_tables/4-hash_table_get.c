d6667b2c21a05934e53bdfb8e03690db28a1a847
tenyapious <tenyapious@gmail.com>
2023-12-06 15:56:05 +0000

    Return NULL if ht == NULL or key == NULL or key is an empty string

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,6 +12,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *temp;
 
+	if (ht == NULL || key == NULL || strlen(key) == 0)
+		return (NULL);
+
 	index = key_index((unsigned char *)key, ht->size);
 
 	temp = ht->array[index];
