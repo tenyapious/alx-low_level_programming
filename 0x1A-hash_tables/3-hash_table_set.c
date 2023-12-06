3667c43e92f852ff06b73195f4f023ebc2b85d56
tenyapious <tenyapious@gmail.com>
2023-12-06 13:38:36 +0000

    put instructions on separate lines

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,7 +24,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((unsigned char *)key, ht->size);
 
 	keyDup = strdup(key);
-	valueDup = strdup(value);temp = ht->array[index];
+	valueDup = strdup(value);
+	temp = ht->array[index];
 
 	while (temp)
 	{
